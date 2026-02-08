17d3203fa4e583ba4f3971c67cdabbab1e3ca345
Aristides71 <agomes.bel71@gmail.com>
2026-02-08 11:41:01 -0300

    fix: downgrade express to v4, fix catch-all route, add auth interceptor

--- server/server.js
+++ server/server.js
@@ -358,7 +358,7 @@ app.delete('/api/bank-accounts/:id', authenticateToken, async (req, res) => {
 // Serve static files from React app
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
-app.get(/(.*)/, (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
